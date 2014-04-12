5746271ffab6eb54161bb8707890b47ec709c6bc
giaf <gianfri2008@hotmail.it>
2014-04-12 16:38:05 +0200

    added kernel sgemm atom 4x2

--- kernel/kernel_sgemm_atom_lib4.c
+++ kernel/kernel_sgemm_atom_lib4.c
@@ -635,6 +635,194 @@ void kernel_sgemm_pp_nt_4x2_c99_lib4(int kmax, float *A, float *B, float *C, int
 
 
 
+// 4x4 with data packed in 4
+void kernel_sgemm_pp_nt_4x2_atom_lib4(int kmax, float *A, float *B, float *C, int ldc_dummy, int alg)
+	{
+	
+	if(kmax<=0)
+		return;
+	
+	int k_iter = kmax/4;
+	int k_left = kmax%4;
+
+	__asm__ volatile
+	(
+		"                                \n\t"
+		"                                \n\t"
+		"movl   %2, %%eax                \n\t" // load address of A
+		"movl   %3, %%edx                \n\t" // load address of B
+		"                                \n\t"
+		"                                \n\t"
+		"prefetcht0 0(%%eax)             \n\t"
+		"prefetcht0 0(%%edx)             \n\t"
+		"prefetcht0 64(%%eax)            \n\t"
+		"prefetcht0 64(%%edx)            \n\t"
+		"                                \n\t"
+		"                                \n\t" // zero registers
+		"movaps (%%edx), %%xmm0          \n\t" // B[0]
+		"                                \n\t"
+		"                                \n\t" // zero registers
+		"xorps  %%xmm2, %%xmm2           \n\t" //
+		"movaps %%xmm2, %%xmm1           \n\t" //
+		"movaps %%xmm2, %%xmm3           \n\t" //
+		"movaps %%xmm2, %%xmm4           \n\t" // c_03_0_a
+		"movaps %%xmm2, %%xmm5           \n\t" // c_03_1_a
+		"movaps %%xmm2, %%xmm6           \n\t" // c_03_0_b
+		"movaps %%xmm2, %%xmm7           \n\t" // c_03_1_b
+		"                                \n\t"
+		"                                \n\t"
+		"                                \n\t"
+		"                                \n\t"
+		"movl   %0, %%ecx                \n\t" // i = k_iter;
+		"testl  %%ecx, %%ecx             \n\t" // check i
+		"je     .DCONSIDKLEFT2            \n\t" // if i==0, jump to k_left loop
+		"                                \n\t"
+		"                                \n\t"
+		".DLOOPKITER2:                   \n\t" // MAIN LOOP
+		"                                \n\t"
+		"                                \n\t"
+		"prefetcht0 128(%%eax)           \n\t"
+		"prefetcht0 128(%%edx)           \n\t"
+		"                                \n\t"
+		"                                \n\t"
+		"addps	%%xmm1, %%xmm5           \n\t"
+		"movaps	%%xmm0, %%xmm1           \n\t"
+		"shufps	$0, %%xmm0, %%xmm0       \n\t" // b_0
+		"mulps	0(%%eax), %%xmm0         \n\t"
+		"addps	%%xmm2, %%xmm6           \n\t"
+		"movaps	16(%%edx), %%xmm2        \n\t" // b_k+1
+		"shufps	$85, %%xmm1, %%xmm1      \n\t" // b_1
+		"mulps	0(%%eax), %%xmm1         \n\t"
+		"addps	%%xmm3, %%xmm7           \n\t"
+		"movaps	%%xmm2, %%xmm3           \n\t"
+		"shufps	$0, %%xmm2, %%xmm2       \n\t" // b_0
+		"mulps	16(%%eax), %%xmm2        \n\t"
+		"addps	%%xmm0, %%xmm4           \n\t"
+		"movaps	32(%%edx), %%xmm0        \n\t" // b_k+2
+		"shufps	$85, %%xmm3, %%xmm3      \n\t" // b_1
+		"mulps	16(%%eax), %%xmm3        \n\t"
+		"                                \n\t"
+		"decl   %%ecx                    \n\t" // i -= 1;
+		"                                \n\t"
+		"addps	%%xmm1, %%xmm5           \n\t"
+		"movaps	%%xmm0, %%xmm1           \n\t"
+		"shufps	$0, %%xmm0, %%xmm0       \n\t" // b_0
+		"mulps	32(%%eax), %%xmm0        \n\t"
+		"addps	%%xmm2, %%xmm6           \n\t"
+		"movaps	32(%%edx), %%xmm2        \n\t" // b_k+1
+		"shufps	$85, %%xmm1, %%xmm1      \n\t" // b_1
+		"leal	64(%%edx), %%edx         \n\t"
+		"mulps	32(%%eax), %%xmm1        \n\t"
+		"addps	%%xmm3, %%xmm7           \n\t"
+		"movaps	%%xmm2, %%xmm3           \n\t"
+		"shufps	$0, %%xmm2, %%xmm2       \n\t" // b_0
+		"mulps	48(%%eax), %%xmm2        \n\t"
+		"addps	%%xmm0, %%xmm4           \n\t"
+		"movaps	0(%%edx), %%xmm0        \n\t" // b_k+2
+		"shufps	$85, %%xmm3, %%xmm3      \n\t" // b_1
+		"mulps	48(%%eax), %%xmm3        \n\t"
+		"leal	64(%%eax), %%eax         \n\t"
+		"                                \n\t"
+		"                                \n\t"
+		"jne    .DLOOPKITER2             \n\t" // iterate again if i != 0.
+		"                                \n\t"
+		"                                \n\t"
+		"addps	%%xmm1, %%xmm5           \n\t"
+		"addps	%%xmm2, %%xmm6           \n\t"
+		"addps	%%xmm3, %%xmm7           \n\t"
+		"                                \n\t"
+		"                                \n\t"
+		".DCONSIDKLEFT2:                 \n\t"
+		"                                \n\t" // else, we prepare to enter k_left loop.
+		"                                \n\t"
+		"movl   %1, %%ecx                \n\t" // i = k_left;
+		"testl  %%ecx, %%ecx             \n\t" // check i via logical AND.
+		"je     .DPOSTACCUM2             \n\t" // if i == 0, we're done; jump to end.
+		"                                \n\t" // else, we prepare to enter k_left loop.
+		"                                \n\t"
+		"                                \n\t"
+		".DLOOPKLEFT2:                   \n\t" // EDGE LOOP
+		"                                \n\t"
+		"                                \n\t"
+		"movaps	%%xmm0, %%xmm1           \n\t"
+		"shufps	$0, %%xmm0, %%xmm0       \n\t" // b_0
+		"mulps	0(%%eax), %%xmm0         \n\t"
+		"addps	%%xmm2, %%xmm6           \n\t"
+		"movaps	16(%%edx), %%xmm2        \n\t" // b_k+1
+		"shufps	$85, %%xmm1, %%xmm1      \n\t" // b_1
+		"mulps	0(%%eax), %%xmm1         \n\t"
+		"decl   %%ecx                    \n\t" // i -= 1;
+		"leal	16(%%edx), %%edx         \n\t"
+		"addps	%%xmm0, %%xmm4           \n\t"
+		"leal	16(%%eax), %%eax         \n\t"
+		"addps	%%xmm1, %%xmm5           \n\t"
+		"                                \n\t"
+		"                                \n\t"
+		"jne    .DLOOPKLEFT2             \n\t" // iterate again if i != 0.
+		"                                \n\t"
+		"                                \n\t"
+		"                                \n\t"
+		".DPOSTACCUM2:                   \n\t"
+		"                                \n\t"
+		"addps	%%xmm6, %%xmm4           \n\t"
+		"addps	%%xmm7, %%xmm5           \n\t"
+		"                                \n\t"
+		"                                \n\t"
+		"movl   %4, %%eax                \n\t" // load address of C
+		"                                \n\t"
+		"                                \n\t"
+		"movl   %5, %%ecx                \n\t" // alg
+		"testl  %%ecx, %%ecx             \n\t" // check alg
+		"je     .DZERO2                  \n\t" // if alg==0, jump
+		"                                \n\t"
+		"cmpl	$1, %%ecx                \n\t"
+		"movaps  (%%eax), %%xmm0         \n\t"
+		"movaps  16(%%eax), %%xmm1       \n\t"
+		"je     .DONE2                   \n\t" // if alg==1, jump
+		"                                \n\t"
+		"                                \n\t"// alg==-1
+		"subps  %%xmm4, %%xmm0           \n\t"
+		"subps  %%xmm5, %%xmm1           \n\t"
+		"                                \n\t"
+		"movaps  %%xmm0, (%%eax)         \n\t"
+		"movaps  %%xmm1, 16(%%eax)       \n\t"
+		"                                \n\t"
+		"jmp    .DEND2                   \n\t" // jump to end
+		"                                \n\t"
+		"                                \n\t"
+		"                                \n\t"
+		".DONE2:                         \n\t" // alg==1
+		"                                \n\t"
+		"addps  %%xmm0, %%xmm4           \n\t"
+		"addps  %%xmm1, %%xmm5           \n\t"
+		"                                \n\t"
+		".DZERO2:                        \n\t" // alg==0
+		"                                \n\t"
+		"movaps	%%xmm4, (%%eax)          \n\t"
+		"movaps	%%xmm5, 16(%%eax)        \n\t"
+		"                                \n\t"
+		"                                \n\t"
+		"                                \n\t"
+		".DEND2 :                        \n\t" // end
+		"                                \n\t"
+		: // output operands (none)
+		: // input operands
+		  "m" (k_iter),		// %0
+		  "m" (k_left),		// %1
+		  "m" (A),			// %2
+		  "m" (B),			// %3
+		  "m" (C),			// %4
+		  "m" (alg)			// %5
+		: // register clobber list
+		  "eax", "edx", "ecx",
+		  "xmm0", "xmm1", "xmm2", "xmm3",
+		  "xmm4", "xmm5", "xmm6", "xmm7",
+		  "memory"
+	);
+}
+
+
+
 // normal-transposed, 4x1 with data packed in 4
 void kernel_sgemm_pp_nt_4x1_c99_lib4(int kmax, float *A, float *B, float *C, int ldc, int alg)
 	{
@@ -757,4 +945,5 @@ void kernel_sgemm_pp_nt_4x1_c99_lib4(int kmax, float *A, float *B, float *C, int
 		}
 
 	}
+
 
