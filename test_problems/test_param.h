399d8d607544f50f6a27bec8c76ec327a8054776
giaf <gianfri2008@hotmail.it>
2014-04-29 20:09:30 +0200

    updated readme

--- test_problems/test_param.h
+++ test_problems/test_param.h
@@ -33,10 +33,10 @@
 #define NB 2*NU+2*NX
 
 // number of repetition for timing
-#define NREP 100
+#define NREP 1000
 
 // print results
-#define PRINTRES 1
+#define PRINTRES 0
 
 // print statistics
 #define PRINTSTAT 1
