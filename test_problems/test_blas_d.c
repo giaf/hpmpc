e599c74f69dec8389a1ef21483cf581fa61d4a70
giaf <gianfri2008@hotmail.it>
2014-06-09 14:17:14 +0200

    optimized kernel sgemm neon cortex-a15

--- test_problems/test_blas_d.c
+++ test_problems/test_blas_d.c
@@ -39,7 +39,7 @@ int main()
 	{
 		
 	// maximum frequency of the processor
-	const float GHz_max = 2.9; //3.6; //2.9;
+	const float GHz_max = 2.3; //3.6; //2.9;
 
 	// maximum flops per cycle, double precision
 #if defined(TARGET_X64_AVX)
