f91d5809a1e1fbe53388ddd0dafc19a9ca2a716e
giaf <gianfri2008@hotmail.it>
2014-06-23 15:01:52 +0200

    moved CFLAGS to Makefile.runle & cleaned test ip

--- Makefile
+++ Makefile
@@ -31,66 +31,75 @@ AUX_OBJS = ./auxiliary/aux_d_c99_lib4.o ./auxiliary/aux_s_c99_lib4.o ./auxiliary
 KERNEL_OBJS_DOUBLE = ./kernel/kernel_dgemm_avx_lib4.o ./kernel/kernel_dpotrf_sse3_lib4.o ./kernel/kernel_dgemv_avx_lib4.o ./kernel/corner_dtrmm_avx_lib4.o ./kernel/corner_dpotrf_sse2_lib4.o ./kernel/kernel_dsymv_avx_lib4.o 
 KERNEL_OBJS_SINGLE = ./kernel/kernel_sgemm_avx_lib4.o ./kernel/kernel_spotrf_sse_lib4.o ./kernel/kernel_sgemv_sse_lib4.o ./kernel/corner_strmm_sse_lib4.o ./kernel/corner_spotrf_c99_lib4.o ./kernel/kernel_ssymv_sse_lib4.o
 BLAS_OBJS = ./blas/blas_d_lib4.o ./blas/blas_s_lib4.o
-CFLAGS = $(OPT) -std=c99 -mavx -DTARGET_X64_AVX $(DEBUG)
 endif
 ifeq ($(TARGET), X64_SSE3)
 AUX_OBJS = ./auxiliary/aux_d_c99_lib4.o ./auxiliary/aux_s_c99_lib4.o ./auxiliary/block_size_x64_sse.o 
 KERNEL_OBJS_DOUBLE = ./kernel/kernel_dgemm_sse3_lib4.o ./kernel/kernel_dpotrf_sse3_lib4.o ./kernel/kernel_dgemv_sse3_lib4.o ./kernel/corner_dtrmm_sse3_lib4.o ./kernel/corner_dpotrf_sse2_lib4.o ./kernel/kernel_dsymv_c99_lib4.o 
 KERNEL_OBJS_SINGLE = ./kernel/kernel_sgemm_sse_lib4.o ./kernel/kernel_spotrf_sse_lib4.o ./kernel/kernel_sgemv_sse_lib4.o ./kernel/corner_strmm_sse_lib4.o ./kernel/corner_spotrf_c99_lib4.o ./kernel/kernel_ssymv_sse_lib4.o
 BLAS_OBJS = ./blas/blas_d_lib4.o ./blas/blas_s_lib4.o
-CFLAGS = $(OPT) -std=c99 -msse3 -DTARGET_X64_SSE3 $(DEBUG)
 endif
 ifeq ($(TARGET), AMD_SSE3)
 AUX_OBJS = ./auxiliary/aux_d_c99_lib4.o ./auxiliary/aux_s_c99_lib4.o ./auxiliary/block_size_x64_sse.o 
 KERNEL_OBJS_DOUBLE = ./kernel/kernel_dgemm_amd_sse3_lib4.o ./kernel/kernel_dpotrf_sse3_lib4.o ./kernel/kernel_dgemv_sse3_lib4.o ./kernel/corner_dtrmm_sse3_lib4.o ./kernel/corner_dpotrf_sse2_lib4.o ./kernel/kernel_dsymv_c99_lib4.o 
 KERNEL_OBJS_SINGLE = ./kernel/kernel_sgemm_sse_lib4.o ./kernel/kernel_spotrf_sse_lib4.o ./kernel/kernel_sgemv_sse_lib4.o ./kernel/corner_strmm_sse_lib4.o ./kernel/corner_spotrf_c99_lib4.o ./kernel/kernel_ssymv_sse_lib4.o
 BLAS_OBJS = ./blas/blas_d_lib4.o ./blas/blas_s_lib4.o
-CFLAGS = $(OPT) -std=c99 -msse3 -DTARGET_AMD_SSE3 $(DEBUG)
 endif
 ifeq ($(TARGET), NEON)
 AUX_OBJS = ./auxiliary/aux_d_c99_lib4.o ./auxiliary/aux_s_c99_lib4.o ./auxiliary/block_size_neon.o 
 KERNEL_OBJS_DOUBLE = ./kernel/kernel_dgemm_neon_lib4.o ./kernel/kernel_dpotrf_c99_lib4.o ./kernel/kernel_dgemv_c99_lib4.o ./kernel/corner_dtrmm_c99_lib4.o ./kernel/corner_dpotrf_c99_lib4.o ./kernel/kernel_dsymv_c99_lib4.o 
 KERNEL_OBJS_SINGLE = ./kernel/kernel_sgemm_neon_lib4.o ./kernel/kernel_spotrf_c99_lib4.o ./kernel/kernel_sgemv_c99_lib4.o ./kernel/corner_strmm_c99_lib4.o ./kernel/corner_spotrf_c99_lib4.o ./kernel/kernel_ssymv_c99_lib4.o
 BLAS_OBJS = ./blas/blas_d_lib4.o ./blas/blas_s_lib4.o
-CFLAGS = $(OPT) -std=c99 -fPIC -marm -mfloat-abi=softfp -mfpu=neon -mcpu=cortex-a9 -DTARGET_NEON $(DEBUG)
 endif
 ifeq ($(TARGET), POWERPC_G2)
 AUX_OBJS = ./auxiliary/aux_d_c99_lib4.o ./auxiliary/aux_s_c99_lib4.o ./auxiliary/block_size_ppc.o 
 KERNEL_OBJS_DOUBLE = ./kernel/kernel_dgemm_ppc_g2_lib4.o ./kernel/kernel_dpotrf_c99_lib4.o ./kernel/kernel_dgemv_c99_lib4.o ./kernel/corner_dtrmm_c99_lib4.o ./kernel/corner_dpotrf_c99_lib4.o ./kernel/kernel_dsymv_c99_lib4.o 
 KERNEL_OBJS_SINGLE = ./kernel/kernel_sgemm_ppc_g2_lib4.o ./kernel/kernel_spotrf_c99_lib4.o ./kernel/kernel_sgemv_c99_lib4.o ./kernel/corner_strmm_c99_lib4.o ./kernel/corner_spotrf_c99_lib4.o ./kernel/kernel_ssymv_c99_lib4.o
 BLAS_OBJS = ./blas/blas_d_lib4.o ./blas/blas_s_lib4.o
-CFLAGS = $(OPT) -std=c99 -fPIC -mcpu=603e -DTARGET_POWERPC_G2 $(DEBUG)
 endif
 ifeq ($(TARGET), C99_4X4)
 AUX_OBJS = ./auxiliary/aux_d_c99_lib4.o ./auxiliary/aux_s_c99_lib4.o ./auxiliary/block_size_c99_4x4.o 
 KERNEL_OBJS_DOUBLE = ./kernel/kernel_dgemm_c99_lib4.o ./kernel/kernel_dpotrf_c99_lib4.o ./kernel/kernel_dgemv_c99_lib4.o ./kernel/corner_dtrmm_c99_lib4.o ./kernel/corner_dpotrf_c99_lib4.o ./kernel/kernel_dsymv_c99_lib4.o 
 KERNEL_OBJS_SINGLE = ./kernel/kernel_sgemm_c99_lib4.o ./kernel/kernel_spotrf_c99_lib4.o ./kernel/kernel_sgemv_c99_lib4.o ./kernel/corner_strmm_c99_lib4.o ./kernel/corner_spotrf_c99_lib4.o ./kernel/kernel_ssymv_c99_lib4.o
 BLAS_OBJS = ./blas/blas_d_lib4.o ./blas/blas_s_lib4.o
-CFLAGS = $(OPT) -std=c99 -fPIC -DTARGET_C99_4X4 $(DEBUG)
 endif
 ifeq ($(TARGET), C99_2X2)
 AUX_OBJS = ./auxiliary/aux_d_c99_lib2.o ./auxiliary/aux_s_c99_lib2.o ./auxiliary/block_size_c99_2x2.o 
 KERNEL_OBJS_DOUBLE = ./kernel/kernel_dgemm_c99_lib2.o ./kernel/kernel_dpotrf_c99_lib2.o ./kernel/kernel_dgemv_c99_lib2.o ./kernel/corner_dtrmm_c99_lib2.o ./kernel/corner_dpotrf_c99_lib2.o ./kernel/kernel_dsymv_c99_lib2.o
 KERNEL_OBJS_SINGLE = ./kernel/kernel_sgemm_c99_lib2.o ./kernel/kernel_spotrf_c99_lib2.o ./kernel/kernel_sgemv_c99_lib2.o ./kernel/corner_strmm_c99_lib2.o ./kernel/corner_spotrf_c99_lib2.o ./kernel/kernel_ssymv_c99_lib2.o
 BLAS_OBJS = ./blas/blas_d_lib2.o ./blas/blas_s_lib2.o
-CFLAGS = $(OPT) -std=c99 -fPIC -DTARGET_C99_2X2 $(DEBUG)
 endif
 ifeq ($(TARGET), X86_ATOM)
 AUX_OBJS = ./auxiliary/aux_d_c99_lib2.o ./auxiliary/aux_s_c99_lib2.o ./auxiliary/block_size_x86_atom.o 
 KERNEL_OBJS_DOUBLE = ./kernel/kernel_dgemm_x86_atom_lib2.o ./kernel/kernel_dpotrf_c99_lib2.o ./kernel/kernel_dgemv_c99_lib2.o ./kernel/corner_dtrmm_c99_lib2.o ./kernel/corner_dpotrf_c99_lib2.o ./kernel/kernel_dsymv_c99_lib2.o
 KERNEL_OBJS_SINGLE = ./kernel/kernel_sgemm_x86_atom_lib4.o ./kernel/kernel_spotrf_c99_lib4.o ./kernel/kernel_sgemv_x86_atom_lib4.o ./kernel/corner_strmm_c99_lib4.o ./kernel/corner_spotrf_c99_lib4.o ./kernel/kernel_ssymv_c99_lib4.o
 BLAS_OBJS = ./blas/blas_d_lib2.o ./blas/blas_s_lib4.o
-CFLAGS = $(OPT) -std=c99 -fPIC -msse3 -mfpmath=sse -march=atom -DTARGET_X86_ATOM $(DEBUG)
 endif
 LQCP_OBJS = ./lqcp_solvers/dricposv.o ./lqcp_solvers/sricposv.o ./lqcp_solvers/dres.o ./lqcp_solvers/sres.o
 LQCP_CODEGEN_OBJS = ./codegen/dricposv_codegen.o ./codegen/sricposv_codegen.o  ./codegen/dres_codegen.o ./codegen/sres_codegen.o 
 MPC_OBJS = ./mpc_solvers/d_ip_box.o ./mpc_solvers/d_ip2_box.o ./mpc_solvers/dres_ip_box.o ./mpc_solvers/s_ip_box.o ./mpc_solvers/s_ip2_box.o ./mpc_solvers/sres_ip_box.o
 
-all: clean library test_problem run
+all: notice clean library test_problem run
 
-codegen: clean codegenerator test_problem run
+codegen: notice clean codegenerator test_problem run
+
+notice:
+	@echo
+	@echo
+	@echo 
+	@echo " HPMPC -- Library for High-Performance implementation of solvers for MPC."
+	@echo " Copyright (C) 2014 by Technical University of Denmark. All rights reserved."
+	@echo
+	@echo " HPMPC is distributed in the hope that it will be useful,"
+	@echo " but WITHOUT ANY WARRANTY; without even the implied warranty of"
+	@echo " MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE."
+	@echo " See the GNU Lesser General Public License for more details."
+	@echo
+	@echo
 
 library:
+	@echo
+	@echo " Building HPMPC library..."
+	@echo
 	make -C auxiliary obj
 	make -C kernel obj
 	make -C blas obj
@@ -98,10 +107,13 @@ library:
 	make -C mpc_solvers obj
 	ar rcs libhpmpc.a $(AUX_OBJS) $(KERNEL_OBJS_DOUBLE) $(KERNEL_OBJS_SINGLE) $(BLAS_OBJS) $(LQCP_OBJS) $(MPC_OBJS)
 	@echo
-	@echo " libhpmpc.a library build complete."
+	@echo " HPMPC library build completed."
 	@echo
 
 codegenerator:
+	@echo
+	@echo " Building HPMPC code generator..."
+	@echo
 	make -C auxiliary obj
 	make -C kernel obj
 	make -C blas obj
@@ -113,30 +125,52 @@ codegenerator:
 	make -C mpc_solvers obj
 	ar rcs libhpmpc.a $(AUX_OBJS) $(KERNEL_OBJS_DOUBLE) $(KERNEL_OBJS_SINGLE) $(LQCP_CODEGEN_OBJS) $(BLAS_OBJS) $(MPC_OBJS)
 	@echo
-	@echo " libhpmpc.a code generator build complete."
+	@echo " HPMPC code generator build completed."
 	@echo
 
 test_problem:
+	@echo
+	@echo " Building test problem..."
+	@echo
 	cp libhpmpc.a ./test_problems/libhpmpc.a
 #	cp HPMPC.a ./matlab/HPMPC.a
 	make -C test_problems obj
 	@echo
-	@echo " Test problem build complete."
+	@echo " Test problem build completed."
 	@echo
 
 run:
+	@echo
+	@echo " Running test problem..."
+	@echo
 	./test_problems/test.out
 
 #install: library
 install:
-	cp -f libhpmpc.a /usr/local/lib/libhpmpc.a
-	cp -rf ./include /usr/local/include/hpmpc
+	@echo
+	@echo " Installing HPMPC library..."
+	@echo
+	cp -f libhpmpc.a /usr/lib/libhpmpc.a
+	mkdir -p /usr/include/hpmpc
+	cp -rf ./include/* /usr/include/hpmpc
+	@echo
+	@echo " HPMPC library installation completed"
+	@echo
 	
 uninstall:
+	@echo
+	@echo " Uninstalling HPMPC library..."
+	@echo
 	rm /lib/libhpmpc.a
 	rm -r /include/hpmpc
+	@echo
+	@echo " HPMPC library uninstallation completed"
+	@echo
 	
 clean:
+	@echo
+	@echo " Cleaning HPMPC library..."
+	@echo
 	make -C auxiliary clean
 	make -C kernel clean
 	make -C blas clean
@@ -145,10 +179,12 @@ clean:
 	make -C codegen clean
 	make -C test_problems clean
 	make -C interfaces/octave clean
-#	rm -f $(OBJS)
 	rm -f test.out
 	rm -f *.s
 	rm -f *.o
 	rm -f libhpmpc.a
 #	rm -f ./matlab/HPMPC.a
+	@echo
+	@echo " HPMPC library clean completed."
+	@echo
 
--- test_problems/test_d_ip_box.c
+++ test_problems/test_d_ip_box.c
@@ -41,16 +41,6 @@
 
 
 
-/*void openblas_set_num_threads(int num_threads);*/
-
-/*void dgesv_(int *n, int *nrhs, double *A, int *lda, int *ipiv, double *B, int *ldb, int *info);*/
-/*void dgemm_(char *transa, char *transb, int *m, int *n, int *k, double *alpha, double *A, int *lda, double *B, int *ldb, double *beta, double *C, int *ldc);*/
-/*void dcopy_(int *n, double *dx, int *incx, double *dy, int *incy);*/
-/*void daxpy_(int *n, double *da, double *dx, int *incx, double *dy, int *incy);*/
-/*void dscal_(int *n, double *da, double *dx, int *incx);*/
-
-
-
 /************************************************ 
 Mass-spring system: nx/2 masses connected each other with springs (in a row), and the first and the last one to walls. nu (<=nx) controls act on the first nu masses. The system is sampled with sampling time Ts=1. 
 The system seem to give problems for a large number of states: the riccati procedure with OpenBLAS becomes MUCH slower for approximately nx>=1024 (double) and nx>=64 (float). ? under-flow ?
@@ -60,15 +50,9 @@ void mass_spring_system(double Ts, int nx, int nu, int N, double *A, double *B, 
 
 	int nx2 = nx*nx;
 
-//	double d0 = 0;
-//	double d1 = 1;
-//	double dm1 = -1;
-//	int i1 = 1;
 	int info = 0;
-//	char cn = 'n';
 
 	int pp = nx/2; // number of masses
-/*	int mm = nu;   // number of forces*/
 	
 /************************************************
 * build the continuous time system 
@@ -104,21 +88,16 @@ void mass_spring_system(double Ts, int nx, int nu, int N, double *A, double *B, 
 	dmcopy(nx, 1, bb, nx, b, nx);
 		
 	dmcopy(nx, nx, Ac, nx, A, nx);
-//	dscal_(&nx2, &Ts, A, &i1);
 	dscal_3l(nx2, Ts, A);
 	expm(nx, A);
 	
 	d_zeros(&T, nx, nx);
 	d_zeros(&I, nx, nx); for(ii=0; ii<nx; ii++) I[ii*(nx+1)]=1.0; //I = eye(nx);
-//	dcopy_(&nx2, A, &i1, T, &i1);
 	dmcopy(nx, nx, A, nx, T, nx);
-//	daxpy_(&nx2, &dm1, I, &i1, T, &i1);
 	daxpy_3l(nx2, -1.0, I, T);
-//	dgemm_(&cn, &cn, &nx, &nu, &nx, &d1, T, &nx, Bc, &nx, &d0, B, &nx);
 	dgemm_nn_3l(nx, nu, nx, T, nx, Bc, nx, B, nx);
 	
 	int *ipiv = (int *) malloc(nx*sizeof(int));
-//	dgesv_(&nx, &nu, Ac, &nx, ipiv, B, &nx, &info);
 	dgesv_3l(nx, nu, Ac, nx, ipiv, B, nx, &info);
 	free(ipiv);
 
@@ -152,6 +131,20 @@ void mass_spring_system(double Ts, int nx, int nu, int N, double *A, double *B, 
 int main()
 	{
 	
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf(" HPMPC -- Library for High-Performance implementation of solvers for MPC.\n");
+	printf(" Copyright (C) 2014 by Technical University of Denmark. All rights reserved.\n");
+	printf("\n");
+	printf(" HPMPC is distributed in the hope that it will be useful,\n");
+	printf(" but WITHOUT ANY WARRANTY; without even the implied warranty of\n");
+	printf(" MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n");
+	printf(" See the GNU Lesser General Public License for more details.\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+
 #if defined(TARGET_X64_AVX) || defined(TARGET_X64_SSE3) || defined(TARGET_X86_ATOM) || defined(TARGET_AMD_SSE3)
 /*	printf("\nflush subnormals to zero\n");*/
 	_MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON); // flush to zero subnormals !!! works only with one thread !!!
@@ -166,6 +159,18 @@ int main()
 	int N  = NN; // horizon lenght
 	int nb = NB; // number of box constraints (it has to be even, and the first 2*nu constraints are for the inputs, the following for the states)
 
+	printf(" Test problem: mass-spring system with %d masses and %d controls.\n", nx/2, nu);
+	printf("\n");
+	printf(" MPC problem size: %d states, %d inputs, %d horizon length, %d two-sided box constraints.\n", nx, nu, N, nb);
+	printf("\n");
+#if IP == 1
+	printf(" IP method parameters: primal-dual IP, double precision, %d maximum iterations, %e exit tolerance in duality measure.\n", K_MAX, TOL);
+#elif IP == 2
+	printf(" IP method parameters: predictor-corrector IP, double precision, %d maximum iterations, %e exit tolerance in duality measure.\n", K_MAX, TOL);
+#else
+	printf(" Wrong value for IP solver choice: %d\n", IP);
+#endif
+
 	int info = 0;
 		
 	const int dbs = D_MR; //d_get_mr();
@@ -173,8 +178,6 @@ int main()
 	int nz = nx+nu+1;
 	int pnz = dbs*((nz+dbs-nu%dbs+dbs-1)/dbs);
 	
-/*	printf("\n\n%d %d %d %d\n\n", dbs, pnz, sbs, spnz);*/
-
 /************************************************
 * dynamical system
 ************************************************/	
@@ -209,8 +212,6 @@ int main()
 	dmcopy(nx, nx, A, nx, BAb+nu*nx, nx);
 	dmcopy(nx, 1 , b, nx, BAb+(nu+nx)*nx, nx);
 	
-//	d_print_mat(nx, nx+nu+1, BAb, nx);
-
 	/* transposed */
 	double *BAbt; d_zeros_align(&BAbt, pnz, pnz);
 	for(ii=0; ii<nx; ii++)
@@ -219,15 +220,9 @@ int main()
 			BAbt[jj+pnz*ii] = BAb[ii+nx*jj];
 			}
 
-//	d_print_mat(nz, nx+1, BAbt, pnz);
-	
 	/* packed into contiguous memory */
 	double *pBAbt; d_zeros_align(&pBAbt, pnz, pnz);
 	d_cvt_mat2pmat(nz, nx, 0, dbs, BAbt, pnz, pBAbt, pnz);
-
-/*	d_print_pmat(nz, nx, dbs, pBAbt, pnz);*/
-/*	s_print_pmat(nz, nx, sbs, psBAbt, spnz);*/
-/*	return 0;*/
 
 /************************************************
 * box constraints
@@ -253,13 +248,11 @@ int main()
 	for(ii=0; ii<nu; ii++) Q[ii*(pnz+1)] = 2.0;
 	for(; ii<pnz; ii++) Q[ii*(pnz+1)] = 1.0;
 	for(ii=0; ii<nz; ii++) Q[nx+nu+ii*pnz] = 0.1;
-	Q[(nx+nu)*(pnz+1)] = 1e35;
+	Q[(nx+nu)*(pnz+1)] = 1e35; // large enough
 	
 	/* packed into contiguous memory */
 	double *pQ; d_zeros_align(&pQ, pnz, pnz);
 	d_cvt_mat2pmat(nz, nz, 0, dbs, Q, pnz, pQ, pnz);
-
-/*	d_cvt_mat2pmat(nz, nz, 0, dbs, Q, pnz, pQ, pnz);*/
 
 /************************************************
 * matrices series
@@ -304,10 +297,6 @@ int main()
 	
 	// starting guess
 	for(jj=0; jj<nx; jj++) hux[0][nu+jj]=x0[jj];
-	
-/*	double *pL; d_zeros_align(&pL, pnz, pnz);*/
-/*	*/
-/*	double *pBAbtL; d_zeros_align(&pBAbtL, pnz, pnz);*/
 
 /************************************************
 * riccati-like iteration
@@ -315,8 +304,6 @@ int main()
 
 	double *work; d_zeros_align(&work, 2*((N+1)*(pnz*pnz+2*pnz+2*4*nb)+2*pnz*pnz), 1); // work space
 	int kk = 0; // acutal number of iterations
-/*	char prec = PREC; // double/single precision*/
-/*	double sp_thr = SP_THR; // threshold to switch between double and single precision*/
 	int k_max = K_MAX; // maximum number of iterations in the IP method
 	double tol = TOL; // tolerance in the duality measure
 	double sigma[] = {0.4, 0.3, 0.01}; // control primal-dual IP behaviour
@@ -358,6 +345,7 @@ int main()
 		d_ip2_box(&kk, k_max, tol, warm_start, sigma, stat, nx, nu, N, nb, hpBAbt, hpQ, hlb, hub, hux, compute_mult, hpi, hlam, ht, work, &info);
 
 
+	int kk_avg = 0;
 
 	/* timing */
 	struct timeval tv0, tv1;
@@ -384,6 +372,8 @@ int main()
 		else
 			d_ip2_box(&kk, k_max, tol, warm_start, sigma, stat, nx, nu, N, nb, hpBAbt, hpQ, hlb, hub, hux, compute_mult, hpi, hlam, ht, work, &info);
 
+		kk_avg += kk;
+
 		}
 	
 	gettimeofday(&tv1, NULL); // stop
@@ -392,9 +382,15 @@ int main()
 
 	float time = (float) (tv1.tv_sec-tv0.tv_sec)/(nrep+0.0)+(tv1.tv_usec-tv0.tv_usec)/(nrep*1e6);
 	
-	printf("\nnx\tnu\tN\tkernel\n\n");
-	printf("\n%d\t%d\t%d\t%e\n\n", nx, nu, N, time);
+/*	printf("\nnx\tnu\tN\tkernel\n\n");*/
+/*	printf("\n%d\t%d\t%d\t%e\n\n", nx, nu, N, time);*/
 	
+	printf("\n");
+	printf(" Average number of iterations over %d runs: %f\n", nrep, kk_avg / (double) nrep);
+	printf("\n");
+	printf(" Average solution time over %d runs: %e seconds\n", nrep, time);
+	printf("\n");
+
 
 
 	// restore linear part of cost function 
@@ -412,6 +408,11 @@ int main()
 	if(PRINTSTAT==1)
 		{
 
+		printf("\n");
+		printf("\n");
+		printf(" Print IP statistics of the last run\n");
+		printf("\n");
+
 		for(jj=0; jj<kk; jj++)
 			printf("k = %d\tsigma = %f\talpha = %f\tmu = %f\t\tmu = %e\talpha = %f\tmu = %f\tmu = %e\n", jj, stat[5*jj], stat[5*jj+1], stat[5*jj+2], stat[5*jj+2], stat[5*jj+3], stat[5*jj+4], stat[5*jj+4]);
 		printf("\n");
@@ -421,6 +422,11 @@ int main()
 	if(PRINTRES==1)
 		{
 
+		printf("\n");
+		printf("\n");
+		printf(" Print solution\n");
+		printf("\n");
+
 		printf("\nu = \n\n");
 		for(ii=0; ii<N; ii++)
 			d_print_mat(1, nu, hux[ii], 1);
@@ -429,21 +435,29 @@ int main()
 
 	if(PRINTRES==1 && COMPUTE_MULT==1)
 		{
+
 		// print result 
-		printf("\n\nresiduals\n\n");
-		printf("\n\nrq = \n\n");
+		printf("\n");
+		printf("\n");
+		printf(" Print residuals\n\n");
+		printf("\n");
+		printf("\n");
+		printf("rq = \n\n");
 		for(ii=0; ii<=N; ii++)
-/*			d_print_mat_e(1, nx+nu, hrq[ii], 1);*/
 			d_print_mat(1, nx+nu, hrq[ii], 1);
-		printf("\n\nrb = \n\n");
+		printf("\n");
+		printf("\n");
+		printf("rb = \n\n");
 		for(ii=0; ii<N; ii++)
-/*			d_print_mat_e(1, nx, hrb[ii], 1);*/
 			d_print_mat(1, nx, hrb[ii], 1);
-		printf("\n\nrd = \n\n");
+		printf("\n");
+		printf("\n");
+		printf("rd = \n\n");
 		for(ii=0; ii<=N; ii++)
-/*			d_print_mat_e(1, 2*nb, hrd[ii], 1);*/
 			d_print_mat(1, 2*nb, hrd[ii], 1);
-		printf("\n\nmu = %e\n\n", mu);
+		printf("\n");
+		printf("\n");
+		printf("mu = %e\n\n", mu);
 		
 		}
 
