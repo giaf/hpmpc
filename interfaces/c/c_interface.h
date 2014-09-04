4d964093cd5a31eb1dd5400d52e1ee830eae497f
giaf <gianfri2008@hotmail.it>
2014-09-04 13:42:34 +0200

    added c interfaces header

---
+++ interfaces/c/c_interface.h
@@ -0,0 +1,27 @@
+// dynamic memory allocation & c (or row-major) order
+
+//int c_order_dynamic_mem_ip_wrapper( int k_max, double tol, const int nx, const int nu, const int N, double* A, double* B, double* b, double* Q, double* Qf, double* S, double* R, double* q, double* qf, double* r, double* lb, double* ub, double* x, double* u, int* nIt, double *stat );
+int c_order_dynamic_mem_riccati_wrapper_init( const int nx, const int nu, const int N, double *A, double *B, double *b, double *Q, double *Qf, double *S, double *R, double *q, double *qf, double *r, double **ptr_work );
+int c_order_dynamic_mem_riccati_wrapper_free( double *work );
+int c_order_dynamic_mem_riccati_wrapper_fact_solve( const int nx, const int nu, const int N, double *x, double *u, double *pi, double *work );
+int c_order_dynamic_mem_riccati_wrapper_solve( const int nx, const int nu, const int N, double *b, double *q, double *qf, double *r, double *x, double *u, double *pi, double *work );
+
+
+
+// dynamic memory allocation & fortran (or column-major) order
+
+//int fortran_order_dynamic_mem_ip_wrapper( int k_max, double tol, const int nx, const int nu, const int N, double* A, double* B, double* b, double* Q, double* Qf, double* S, double* R, double* q, double* qf, double* r, double* lb, double* ub, double* x, double* u, int* nIt, double *stat );
+int fortran_order_dynamic_mem_riccati_wrapper_init( const int nx, const int nu, const int N, double *A, double *B, double *b, double *Q, double *Qf, double *S, double *R, double *q, double *qf, double *r, double **ptr_work );
+int fortran_order_dynamic_mem_riccati_wrapper_free( double *work );
+int fortran_order_dynamic_mem_riccati_wrapper_fact_solve( const int nx, const int nu, const int N, double *x, double *u, double *pi, double *work );
+int fortran_order_dynamic_mem_riccati_wrapper_solve( const int nx, const int nu, const int N, double *b, double *q, double *qf, double *r, double *x, double *u, double *pi, double *work );
+
+
+
+// static memory allocation & c (or row-major) order
+
+
+
+
+// static memory allocation & fortran (or column-major) order
+
