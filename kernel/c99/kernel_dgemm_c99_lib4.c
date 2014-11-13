6fac77716816385774480c52464d44f95dc91d96
giaf <gianfri2008@hotmail.it>
2014-11-13 12:23:01 +0100

    fixed bug in kernel dgemm c99

--- kernel/c99/kernel_dgemm_c99_lib4.c
+++ kernel/c99/kernel_dgemm_c99_lib4.c
@@ -26,7 +26,7 @@
 
 
 // normal-transposed, 4x4 with data packed in 4
-void kernel_dgemm_nt_4x4_lib4(int kmax, double *A, double *B, double *C, double *D, int alg)
+void kernel_dgemm_nt_4x4_lib4(int kmax, double *A, double *B, double *D, double *C, int alg)
 	{
 	
 	if(kmax<=0)
@@ -310,25 +310,25 @@ void kernel_dgemm_nt_4x4_lib4(int kmax, double *A, double *B, double *C, double 
 			d_33 -= c_33;
 			}
 
-		C[0+ldc*0] = c_00;
-		C[1+ldc*0] = c_10;
-		C[2+ldc*0] = c_20;
-		C[3+ldc*0] = c_30;
+		C[0+ldc*0] = d_00;
+		C[1+ldc*0] = d_10;
+		C[2+ldc*0] = d_20;
+		C[3+ldc*0] = d_30;
 
-		C[0+ldc*1] = c_01;
-		C[1+ldc*1] = c_11;
-		C[2+ldc*1] = c_21;
-		C[3+ldc*1] = c_31;
+		C[0+ldc*1] = d_01;
+		C[1+ldc*1] = d_11;
+		C[2+ldc*1] = d_21;
+		C[3+ldc*1] = d_31;
 
-		C[0+ldc*2] = c_02;
-		C[1+ldc*2] = c_12;
-		C[2+ldc*2] = c_22;
-		C[3+ldc*2] = c_32;
+		C[0+ldc*2] = d_02;
+		C[1+ldc*2] = d_12;
+		C[2+ldc*2] = d_22;
+		C[3+ldc*2] = d_32;
 
-		C[0+ldc*3] = c_03;
-		C[1+ldc*3] = c_13;
-		C[2+ldc*3] = c_23;
-		C[3+ldc*3] = c_33;
+		C[0+ldc*3] = d_03;
+		C[1+ldc*3] = d_13;
+		C[2+ldc*3] = d_23;
+		C[3+ldc*3] = d_33;
 		}
 	
 	}
@@ -336,7 +336,7 @@ void kernel_dgemm_nt_4x4_lib4(int kmax, double *A, double *B, double *C, double 
 
 
 // normal-transposed, 4x2 with data packed in 4
-void kernel_dgemm_nt_4x2_lib4(int kmax, double *A, double *B, double *C, double *D, int alg)
+void kernel_dgemm_nt_4x2_lib4(int kmax, double *A, double *B, double *D, double *C, int alg)
 	{
 	
 	if(kmax<=0)
@@ -520,15 +520,15 @@ void kernel_dgemm_nt_4x2_lib4(int kmax, double *A, double *B, double *C, double 
 			d_31 -= c_31;
 			}
 
-		C[0+ldc*0] = c_00;
-		C[1+ldc*0] = c_10;
-		C[2+ldc*0] = c_20;
-		C[3+ldc*0] = c_30;
+		C[0+ldc*0] = d_00;
+		C[1+ldc*0] = d_10;
+		C[2+ldc*0] = d_20;
+		C[3+ldc*0] = d_30;
 
-		C[0+ldc*1] = c_01;
-		C[1+ldc*1] = c_11;
-		C[2+ldc*1] = c_21;
-		C[3+ldc*1] = c_31;
+		C[0+ldc*1] = d_01;
+		C[1+ldc*1] = d_11;
+		C[2+ldc*1] = d_21;
+		C[3+ldc*1] = d_31;
 		}
 	
 	}
@@ -536,7 +536,7 @@ void kernel_dgemm_nt_4x2_lib4(int kmax, double *A, double *B, double *C, double 
 
 
 // normal-transposed, 2x4 with data packed in 4
-void kernel_dgemm_nt_2x4_lib4(int kmax, double *A, double *B, double *C, double *D, int alg)
+void kernel_dgemm_nt_2x4_lib4(int kmax, double *A, double *B, double *D, double *C, int alg)
 	{
 	
 	if(kmax<=0)
@@ -734,17 +734,17 @@ void kernel_dgemm_nt_2x4_lib4(int kmax, double *A, double *B, double *C, double 
 			d_13 -= c_13;
 			}
 
-		C[0+ldc*0] = c_00;
-		C[1+ldc*0] = c_10;
+		C[0+ldc*0] = d_00;
+		C[1+ldc*0] = d_10;
 
-		C[0+ldc*1] = c_01;
-		C[1+ldc*1] = c_11;
+		C[0+ldc*1] = d_01;
+		C[1+ldc*1] = d_11;
 
-		C[0+ldc*2] = c_02;
-		C[1+ldc*2] = c_12;
+		C[0+ldc*2] = d_02;
+		C[1+ldc*2] = d_12;
 
-		C[0+ldc*3] = c_03;
-		C[1+ldc*3] = c_13;
+		C[0+ldc*3] = d_03;
+		C[1+ldc*3] = d_13;
 		}
 	
 	}
@@ -752,7 +752,7 @@ void kernel_dgemm_nt_2x4_lib4(int kmax, double *A, double *B, double *C, double 
 
 
 // normal-transposed, 2x2 with data packed in 4
-void kernel_dgemm_nt_2x2_lib4(int kmax, double *A, double *B, double *C, double *D, int alg)
+void kernel_dgemm_nt_2x2_lib4(int kmax, double *A, double *B, double *D, double *C, int alg)
 	{
 	
 	if(kmax<=0)
@@ -886,11 +886,11 @@ void kernel_dgemm_nt_2x2_lib4(int kmax, double *A, double *B, double *C, double 
 			d_11 -= c_11;
 			}
 
-		C[0+ldc*0] = c_00;
-		C[1+ldc*0] = c_10;
+		C[0+ldc*0] = d_00;
+		C[1+ldc*0] = d_10;
 
-		C[0+ldc*1] = c_01;
-		C[1+ldc*1] = c_11;
+		C[0+ldc*1] = d_01;
+		C[1+ldc*1] = d_11;
 		}
 	
 	}
