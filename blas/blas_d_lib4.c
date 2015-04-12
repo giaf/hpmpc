b814bc5eb77128f8337f43786d76d886b59422b8
giaf <gianfri2008@hotmail.it>
2015-04-12 21:28:41 +0200

    workaround bug in kernel avx t 12

--- blas/blas_d_lib4.c
+++ blas/blas_d_lib4.c
@@ -2456,7 +2456,7 @@ void dgemv_t_lib(int m, int n, double *pA, int sda, double *x, double *y, int al
 	int j;
 	
 	j=0;
-#if defined(TARGET_X64_AVX2) || defined(TARGET_X64_AVX)
+#if 0 //defined(TARGET_X64_AVX2) || defined(TARGET_X64_AVX)
 	for(; j<n-11; j+=12)
 		{
 		kernel_dgemv_t_12_lib4(m, pA+j*bs, sda, x, y+j, alg);
