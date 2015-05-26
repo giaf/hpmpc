d444f22a06921e52a307026ae97e96be6592b38c
giaf <gianfri2008@hotmail.it>
2015-05-26 17:20:14 +0200

    added compute Gamma u & compute Gamma u Q

--- lqcp_solvers/d_cond.c
+++ lqcp_solvers/d_cond.c
@@ -29,13 +29,53 @@
 
 
 
-void d_compute_gamma_u(int N, int nx, int nu, double **pA, double **pBt, double **Gamma_u)
+void d_compute_Gamma_u(int N, int nx, int nu, double **pA, double **pBt, double **pGamma_u)
 	{
 
 	const int bs = D_MR;
 	const int ncl = D_NCL;
 
-	int ii;
+	int cnx = (nx+ncl-1)/ncl*ncl;
+
+	int ii, offset;
 
 	// first stage
-	d_copy_
+	d_copy_pmat_general(nu, nx, 0, pBt[0], cnx, 0, pGamma_u[0], cnx);
+
+	// general stage
+	for(ii=1; ii<N; ii++)
+		{
+		offset = ii*nu;
+#if defined(TARGET_X64_AVX2) || defined(TARGET_X64_AVX) || defined(TARGET_C99_4X4)
+		dgemm_nt_lib(nx, ii*nu, nx, pA[ii], cnx, pGamma_u[ii-1], cnx, pGamma_u[ii], cnx, pGamma_u[ii], cnx, 0, 0, 1); // (A * Gamma_u^T)^T
+#else
+		dgemm_nt_lib(ii*nu, nx, nx, pGamma_u[ii-1], cnx, pA[ii], cnx, pGamma_u[ii], cnx, pGamma_u[ii], cnx, 0, 0, 0); // Gamma_u * A^T
+#endif
+		d_copy_pmat_general(nu, nx, 0, pBt[ii], cnx, offset, pGamma_u[ii]+offset/bs*bs*cnx+offset%bs, cnx);
+		}
+	
+	}
+
+
+
+void d_compute_Gamma_u_Q(int N, int nx, int nu, double **pGamma_u, double **pQ, double **pGamma_u_Q)
+	{
+
+	const int bs = D_MR;
+	const int ncl = D_NCL;
+
+	int cnx = (nx+ncl-1)/ncl*ncl;
+
+	int ii;
+
+	// general stage
+	for(ii=0; ii<N; ii++)
+		{
+#if defined(TARGET_X64_AVX2) || defined(TARGET_X64_AVX) || defined(TARGET_C99_4X4)
+		dgemm_nt_lib(nx, (ii+1)*nu, nx, pQ[ii+1], cnx, pGamma_u[ii], cnx, pGamma_u_Q[ii], cnx, pGamma_u_Q[ii], cnx, 0, 0, 1); // (A * Gamma_u^T)^T
+#else
+		dgemm_nt_lib((ii+1)*nu, nx, nx, pGamma_u[ii], cnx, pQ[ii+1], cnx, pGamma_u_Q[ii], cnx, pGamma_u_Q[ii], cnx, 0, 0, 0); // Gamma_u * A^T
+#endif
+		}
+	
+	}
