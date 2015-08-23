1bed6daca191950fb0141ae7ea1f5e94713812b0
giaf <gianfri2008@hotmail.it>
2015-08-23 14:35:44 +0200

    added back ric trf & trs

--- include/lqcp_solvers.h
+++ include/lqcp_solvers.h
@@ -69,3 +69,5 @@ void d_cond_fact_R(int N, int nx, int nu, int nx2_fact, double **pA, double **pA
 
 // new routines
 void d_back_ric_sv_new(int N, int nx, int nu, double **hpBAbt, double **hpQ, int update_hessian, double **hQd, double **hQl, int fixed_x0, double **hux, double **hpL, double **hl, double *pLBAbt, double *work, int compute_Pb, double **hPb, int compute_pi, double **hpi, int nb, int ng, int ngN, double **hpDCt, double **Qx, double **qx);
+void d_back_ric_trf_new(int N, int nx, int nu, double **hpBAbt, double **hpQ, int update_hessian, double **hQd, int fixed_x0, double **hpL, double **hdL, double *pLBAbtDCt, double *work, int nb, int ng, int ngN, double **hpDCt, double **Qx);
+void d_back_ric_trs_new(int N, int nx, int nu, double **hpBAbt, double **hq, int fixed_x0, double **hux, double **hpL, double **hdL, double *work, int compute_Pb, double **hPb, int compute_pi, double **hpi, int nb, int ng, int ngN, double **hpDCt, double **qx);
