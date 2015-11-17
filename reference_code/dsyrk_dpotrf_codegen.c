f5445428ee950df9803e85e14c962361328361e1
giaf <gianfri2008@hotmail.it>
2015-11-17 19:44:31 +0100

    added codegenerated reference code for dsyrk

--- reference_code/dsyrk_dpotrf_codegen.c
+++ reference_code/dsyrk_dpotrf_codegen.c
@@ -25,7 +25,7 @@
 
 #include <math.h>
 
-#define N 0+7*40
+#define N 4+0*40
 
 
 int dpotrf_codegen_0(double *A)
@@ -70,7 +70,7 @@ int dpotrf_codegen_0(double *A)
 
 		for(ii=jj+1; ii<n; ii++)
 			{
-			A[ii+lda*jj] = A[ii+lda*jj] * a_jj;
+			A[ii+lda*jj] *= a_jj;
 			}
 
 		}
@@ -123,7 +123,7 @@ int dpotrf_codegen_1(double *A)
 
 		for(ii=jj+1; ii<n; ii++)
 			{
-			A[ii+lda*jj] = A[ii+lda*jj] * a_jj;
+			A[ii+lda*jj] *= a_jj;
 			}
 
 		}
@@ -176,7 +176,7 @@ int dpotrf_codegen_2(double *A)
 
 		for(ii=jj+1; ii<n; ii++)
 			{
-			A[ii+lda*jj] = A[ii+lda*jj] * a_jj;
+			A[ii+lda*jj] *= a_jj;
 			}
 
 		}
@@ -229,7 +229,7 @@ int dpotrf_codegen_3(double *A)
 
 		for(ii=jj+1; ii<n; ii++)
 			{
-			A[ii+lda*jj] = A[ii+lda*jj] * a_jj;
+			A[ii+lda*jj] *= a_jj;
 			}
 
 		}
@@ -282,7 +282,7 @@ int dpotrf_codegen_4(double *A)
 
 		for(ii=jj+1; ii<n; ii++)
 			{
-			A[ii+lda*jj] = A[ii+lda*jj] * a_jj;
+			A[ii+lda*jj] *= a_jj;
 			}
 
 		}
@@ -335,7 +335,7 @@ int dpotrf_codegen_5(double *A)
 
 		for(ii=jj+1; ii<n; ii++)
 			{
-			A[ii+lda*jj] = A[ii+lda*jj] * a_jj;
+			A[ii+lda*jj] *= a_jj;
 			}
 
 		}
@@ -388,7 +388,7 @@ int dpotrf_codegen_6(double *A)
 
 		for(ii=jj+1; ii<n; ii++)
 			{
-			A[ii+lda*jj] = A[ii+lda*jj] * a_jj;
+			A[ii+lda*jj] *= a_jj;
 			}
 
 		}
@@ -441,7 +441,7 @@ int dpotrf_codegen_7(double *A)
 
 		for(ii=jj+1; ii<n; ii++)
 			{
-			A[ii+lda*jj] = A[ii+lda*jj] * a_jj;
+			A[ii+lda*jj] *= a_jj;
 			}
 
 		}
@@ -494,7 +494,7 @@ int dpotrf_codegen_8(double *A)
 
 		for(ii=jj+1; ii<n; ii++)
 			{
-			A[ii+lda*jj] = A[ii+lda*jj] * a_jj;
+			A[ii+lda*jj] *= a_jj;
 			}
 
 		}
@@ -547,12 +547,471 @@ int dpotrf_codegen_9(double *A)
 
 		for(ii=jj+1; ii<n; ii++)
 			{
-			A[ii+lda*jj] = A[ii+lda*jj] * a_jj;
+			A[ii+lda*jj] *= a_jj;
 			}
 
 		}
 	
 	return 0;
+	
+	}
+
+
+void dsyrk_codegen_0(double *A, double *C)
+	{
+
+	const int n = N+0*4;
+	const int lda = N+0*4;
+
+	double temp;
+
+	int ii, jj, kk;
+
+	for(jj=0; jj<n; jj++)
+		{
+		for(kk=0; kk<n; kk++)
+			{
+			temp = A[jj+lda*kk];
+			for(ii=jj; ii<n; ii++)
+				{
+				C[ii+lda*jj] += temp * A[ii+lda*kk];
+				}
+			}
+		}
+	
+	}
+
+
+
+void dsyrk_codegen_1(double *A, double *C)
+	{
+
+	const int n = N+1*4;
+	const int lda = N+1*4;
+
+	double temp;
+
+	int ii, jj, kk;
+
+	for(jj=0; jj<n; jj++)
+		{
+		for(kk=0; kk<n; kk++)
+			{
+			temp = A[jj+lda*kk];
+			for(ii=jj; ii<n; ii++)
+				{
+				C[ii+lda*jj] += temp * A[ii+lda*kk];
+				}
+			}
+		}
+	
+	}
+
+
+
+void dsyrk_codegen_2(double *A, double *C)
+	{
+
+	const int n = N+2*4;
+	const int lda = N+2*4;
+
+	double temp;
+
+	int ii, jj, kk;
+
+	for(jj=0; jj<n; jj++)
+		{
+		for(kk=0; kk<n; kk++)
+			{
+			temp = A[jj+lda*kk];
+			for(ii=jj; ii<n; ii++)
+				{
+				C[ii+lda*jj] += temp * A[ii+lda*kk];
+				}
+			}
+		}
+	
+	}
+
+
+
+void dsyrk_codegen_3(double *A, double *C)
+	{
+
+	const int n = N+3*4;
+	const int lda = N+3*4;
+
+	double temp;
+
+	int ii, jj, kk;
+
+	for(jj=0; jj<n; jj++)
+		{
+		for(kk=0; kk<n; kk++)
+			{
+			temp = A[jj+lda*kk];
+			for(ii=jj; ii<n; ii++)
+				{
+				C[ii+lda*jj] += temp * A[ii+lda*kk];
+				}
+			}
+		}
+	
+	}
+
+
+
+void dsyrk_codegen_4(double *A, double *C)
+	{
+
+	const int n = N+4*4;
+	const int lda = N+4*4;
+
+	double temp;
+
+	int ii, jj, kk;
+
+	for(jj=0; jj<n; jj++)
+		{
+		for(kk=0; kk<n; kk++)
+			{
+			temp = A[jj+lda*kk];
+			for(ii=jj; ii<n; ii++)
+				{
+				C[ii+lda*jj] += temp * A[ii+lda*kk];
+				}
+			}
+		}
+	
+	}
+
+
+
+void dsyrk_codegen_5(double *A, double *C)
+	{
+
+	const int n = N+5*4;
+	const int lda = N+5*4;
+
+	double temp;
+
+	int ii, jj, kk;
+
+	for(jj=0; jj<n; jj++)
+		{
+		for(kk=0; kk<n; kk++)
+			{
+			temp = A[jj+lda*kk];
+			for(ii=jj; ii<n; ii++)
+				{
+				C[ii+lda*jj] += temp * A[ii+lda*kk];
+				}
+			}
+		}
+	
+	}
+
+
+
+void dsyrk_codegen_6(double *A, double *C)
+	{
+
+	const int n = N+6*4;
+	const int lda = N+6*4;
+
+	double temp;
+
+	int ii, jj, kk;
+
+	for(jj=0; jj<n; jj++)
+		{
+		for(kk=0; kk<n; kk++)
+			{
+			temp = A[jj+lda*kk];
+			for(ii=jj; ii<n; ii++)
+				{
+				C[ii+lda*jj] += temp * A[ii+lda*kk];
+				}
+			}
+		}
+	
+	}
+
+
+
+void dsyrk_codegen_7(double *A, double *C)
+	{
+
+	const int n = N+7*4;
+	const int lda = N+7*4;
+
+	double temp;
+
+	int ii, jj, kk;
+
+	for(jj=0; jj<n; jj++)
+		{
+		for(kk=0; kk<n; kk++)
+			{
+			temp = A[jj+lda*kk];
+			for(ii=jj; ii<n; ii++)
+				{
+				C[ii+lda*jj] += temp * A[ii+lda*kk];
+				}
+			}
+		}
+	
+	}
+
+
+
+void dsyrk_codegen_8(double *A, double *C)
+	{
+
+	const int n = N+8*4;
+	const int lda = N+8*4;
+
+	double temp;
+
+	int ii, jj, kk;
+
+	for(jj=0; jj<n; jj++)
+		{
+		for(kk=0; kk<n; kk++)
+			{
+			temp = A[jj+lda*kk];
+			for(ii=jj; ii<n; ii++)
+				{
+				C[ii+lda*jj] += temp * A[ii+lda*kk];
+				}
+			}
+		}
+	
+	}
+
+
+
+void dsyrk_codegen_9(double *A, double *C)
+	{
+
+	const int n = N+9*4;
+	const int lda = N+9*4;
+
+	double temp;
+
+	int ii, jj, kk;
+
+	for(jj=0; jj<n; jj++)
+		{
+		for(kk=0; kk<n; kk++)
+			{
+			temp = A[jj+lda*kk];
+			for(ii=jj; ii<n; ii++)
+				{
+				C[ii+lda*jj] += temp * A[ii+lda*kk];
+				}
+			}
+		}
+	
+	}
+
+
+
+void dcopy_codegen_0(double *A, double *C)
+	{
+
+	const int n = N+0*4;
+	const int lda = N+0*4;
+
+	int ii, jj;
+
+	for(jj=0; jj<n; jj++)
+		{
+		for(ii=jj; ii<n; ii++)
+			{
+			C[ii+lda*jj] = A[ii+lda*jj];
+			}
+		}
+	
+	}
+
+
+
+void dcopy_codegen_1(double *A, double *C)
+	{
+
+	const int n = N+1*4;
+	const int lda = N+1*4;
+
+	int ii, jj;
+
+	for(jj=0; jj<n; jj++)
+		{
+		for(ii=jj; ii<n; ii++)
+			{
+			C[ii+lda*jj] = A[ii+lda*jj];
+			}
+		}
+	
+	}
+
+
+
+void dcopy_codegen_2(double *A, double *C)
+	{
+
+	const int n = N+2*4;
+	const int lda = N+2*4;
+
+	int ii, jj;
+
+	for(jj=0; jj<n; jj++)
+		{
+		for(ii=jj; ii<n; ii++)
+			{
+			C[ii+lda*jj] = A[ii+lda*jj];
+			}
+		}
+	
+	}
+
+
+
+void dcopy_codegen_3(double *A, double *C)
+	{
+
+	const int n = N+3*4;
+	const int lda = N+3*4;
+
+	int ii, jj;
+
+	for(jj=0; jj<n; jj++)
+		{
+		for(ii=jj; ii<n; ii++)
+			{
+			C[ii+lda*jj] = A[ii+lda*jj];
+			}
+		}
+	
+	}
+
+
+
+void dcopy_codegen_4(double *A, double *C)
+	{
+
+	const int n = N+4*4;
+	const int lda = N+4*4;
+
+	int ii, jj;
+
+	for(jj=0; jj<n; jj++)
+		{
+		for(ii=jj; ii<n; ii++)
+			{
+			C[ii+lda*jj] = A[ii+lda*jj];
+			}
+		}
+	
+	}
+
+
+
+void dcopy_codegen_5(double *A, double *C)
+	{
+
+	const int n = N+5*4;
+	const int lda = N+5*4;
+
+	int ii, jj;
+
+	for(jj=0; jj<n; jj++)
+		{
+		for(ii=jj; ii<n; ii++)
+			{
+			C[ii+lda*jj] = A[ii+lda*jj];
+			}
+		}
+	
+	}
+
+
+
+void dcopy_codegen_6(double *A, double *C)
+	{
+
+	const int n = N+6*4;
+	const int lda = N+6*4;
+
+	int ii, jj;
+
+	for(jj=0; jj<n; jj++)
+		{
+		for(ii=jj; ii<n; ii++)
+			{
+			C[ii+lda*jj] = A[ii+lda*jj];
+			}
+		}
+	
+	}
+
+
+
+void dcopy_codegen_7(double *A, double *C)
+	{
+
+	const int n = N+7*4;
+	const int lda = N+7*4;
+
+	int ii, jj;
+
+	for(jj=0; jj<n; jj++)
+		{
+		for(ii=jj; ii<n; ii++)
+			{
+			C[ii+lda*jj] = A[ii+lda*jj];
+			}
+		}
+	
+	}
+
+
+
+void dcopy_codegen_8(double *A, double *C)
+	{
+
+	const int n = N+8*4;
+	const int lda = N+8*4;
+
+	int ii, jj;
+
+	for(jj=0; jj<n; jj++)
+		{
+		for(ii=jj; ii<n; ii++)
+			{
+			C[ii+lda*jj] = A[ii+lda*jj];
+			}
+		}
+	
+	}
+
+
+
+void dcopy_codegen_9(double *A, double *C)
+	{
+
+	const int n = N+9*4;
+	const int lda = N+9*4;
+
+	int ii, jj;
+
+	for(jj=0; jj<n; jj++)
+		{
+		for(ii=jj; ii<n; ii++)
+			{
+			C[ii+lda*jj] = A[ii+lda*jj];
+			}
+		}
 	
 	}
 
