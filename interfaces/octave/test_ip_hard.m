5c9f9bcc0ea32e1de3ed3c0e452674998cea5841
giaf <gianfri2008@hotmail.it>
2015-12-02 18:36:47 +0100

    nicer test problem octave

--- interfaces/octave/test_ip_hard.m
+++ interfaces/octave/test_ip_hard.m
@@ -13,21 +13,10 @@ end
 nx = 12;			% number of states
 nu = 5;				% number of inputs (controls)
 N = 30;				% horizon length
-if 1
-	nb  = nu+nx;		% number of two-sided box constraints
-	ng  = 0;         % number of two-sided general constraints
-	ngN = nx;         % number of two-sided general constraints on last stage
-else
-	if 0
-		nb  = 0;
-		ng  = nu+nx;
-		ngN = nu+nx;
-	else
-		nb  = nu;
-		ng  = nx;
-		ngN = nx;
-	end
-end
+
+nb  = nu+nx;		% number of two-sided box constraints
+ng  = 0;            % number of two-sided general constraints
+ngN = nx;           % number of two-sided general constraints on last stage
 
 nbu = min(nb, nu);
 
@@ -45,13 +34,8 @@ b = 0.0*ones(nx,1);
 x0 = zeros(nx, 1);
 x0(1) = 3.5;
 x0(2) = 3.5;
-if nx==4
-	x0 = [5 10 15 20]';
-end
 AA = repmat(A, 1, N);
 BB = repmat(B, 1, N);
-%AA = repmat(A', 1, N);
-%BB = repmat(B', 1, N);
 bb = repmat(b, 1, N);
 
 % cost function
@@ -60,10 +44,10 @@ Qf = Q;
 R = 2*eye(nu);
 S = zeros(nx, nu);
 q = zeros(nx,1);
-q = 1*Q*[ones(nx/2,1); zeros(nx/2,1)];
+%q = 1*Q*[ones(nx/2,1); zeros(nx/2,1)];
 qf = q;
 r = zeros(nu,1);
-r = 1*R*ones(nu,1);
+%r = 1*R*ones(nu,1);
 QQ = repmat(Q, 1, N);
 SS = repmat(S, 1, N);
 RR = repmat(R, 1, N);
@@ -74,20 +58,20 @@ rr = repmat(r, 1, N);
 C  = zeros(ng, nx);
 D  = zeros(ng, nu);
 CN = zeros(ngN, nx);
-if(ng>0)
-	if(nb==0)
-		for ii=1:min(nu,ng)
-			D(ii,ii) = 1.0;
-		end
-		for ii=min(nu,ng)+1:ng
-			C(ii,ii-nu) = 1.0;
-		end
-	else
-		for ii=1:ng
-			C(ii,ii) = 1.0;
-		end
-	end
-end
+%if(ng>0)
+%	if(nb==0)
+%		for ii=1:min(nu,ng)
+%			D(ii,ii) = 1.0;
+%		end
+%		for ii=min(nu,ng)+1:ng
+%			C(ii,ii-nu) = 1.0;
+%		end
+%	else
+%		for ii=1:ng
+%			C(ii,ii) = 1.0;
+%		end
+%	end
+%end
 if(ngN>0)
 	for ii=1:ngN
 		CN(ii,ii) = 1.0;
@@ -95,16 +79,13 @@ if(ngN>0)
 end
 CC = [zeros(ng,nx), repmat(C, 1, N-1)];
 DD = [repmat(D, 1, N)];
-%CC = [zeros(nx,ng), repmat(C', 1, N)];
-%DD = [repmat(D', 1, N), zeros(nu,ng)];
 
 % box constraints
 lb = zeros(nb,1);
 ub = zeros(nb,1);
-%db(1:2*nu) = -1.5;
 for ii=1:nbu
-	lb(ii) = -2.5; % lower bound
-	ub(ii) = -0.1; % - upper bound
+	lb(ii) = -0.5; % lower bound
+	ub(ii) =  0.5; % - upper bound
 end
 for ii=nbu+1:nb
 	lb(ii) = -10;
@@ -115,23 +96,23 @@ uub = repmat(ub, 1, N+1);
 % general constraints
 lg = zeros(ng,1);
 ug = zeros(ng,1);
-if(ng>0)
-	if(nb==0)
-		for ii=1:min(nu,ng)
-			lg(ii) = -2.5; % lower bound
-			ug(ii) = -0.1; % - upper bound
-		end
-		for ii=min(nu,ng)+1:ng
-			lg(ii) = -10;
-			ug(ii) =  10;
-		end
-	else
-		for ii=1:ng
-			lg(ii) = -10;
-			ug(ii) =  10;
-		end
-	end
-end
+%if(ng>0)
+%	if(nb==0)
+%		for ii=1:min(nu,ng)
+%			lg(ii) = -2.5; % lower bound
+%			ug(ii) = -0.1; % - upper bound
+%		end
+%		for ii=min(nu,ng)+1:ng
+%			lg(ii) = -10;
+%			ug(ii) =  10;
+%		end
+%	else
+%		for ii=1:ng
+%			lg(ii) = -10;
+%			ug(ii) =  10;
+%		end
+%	end
+%end
 %db(2*nu+1:end) = -4;
 llg = repmat(lg, 1, N);
 uug = repmat(ug, 1, N);
@@ -162,7 +143,7 @@ end
 %end
 
 % initial guess for states and inputs
-x = zeros(nx, N+1); %x(:,1) = x0; % initial condition
+x = zeros(nx, N+1); x(:,1) = x0; % initial condition
 %u = -1*ones(nu, N);
 u = zeros(nu, N);
 %pi = zeros(nx, N+1);
@@ -179,7 +160,7 @@ mult_pi = zeros(nx,N+1);
 mult_lam = zeros(2*(nb+ng)*N+2*(nb+ngN),1);
 mult_t = zeros(2*(nb+ng)*N+2*(nb+ngN),1);
 
-nrep = 100;
+nrep = 1000;
 
 tic
 for ii=1:nrep
